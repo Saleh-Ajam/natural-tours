9401efb5c365d87cb7a73f1c9684766c6e314353
Saleh <saleh-aj@hotmail.com>
2021-09-07 01:36:30 -0700

    Implementing CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,7 +26,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin * 
 // Serving static files
+// to allow front end that is on different domain (natours.com)
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }));
+// non simpler reqwuest for preflight phase
+// app.options(routes, handler)
+app.options('*', cors());
+// app.options('/api/v1/tours/:id',cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Security HTTP headers
