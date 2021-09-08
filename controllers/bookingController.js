e1eddb295dd0510ed203f47e9442ed4609423572
Saleh <saleh-aj@hotmail.com>
2021-09-08 05:15:03 -0700

    Fix bug about amount (price) name

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,7 +51,7 @@ exports.getCheckooutSession = catchAsync(async(req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({email: session.customer_email})).id;
-    const price = session.dsiplay_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({tour, user, price});
 }
 
