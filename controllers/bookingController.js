f4f00546c38e5b7aa20eb848cc72b68083a12dcb
Saleh <saleh-aj@hotmail.com>
2021-09-08 05:43:03 -0700

    Implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckooutSession = catchAsync(async(req, res, next) => {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-        success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
+        success_url: `${req.protocol}://${req.get('host')}/my-bookings?alert=booking`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email, // protected route
         client_reference_id: req.params.tourId,
