fefdaed229df61a6e57929161b26f860c0b70271
Saleh <saleh-aj@hotmail.com>
2021-09-08 01:46:03 -0700

    Imporoved stripe implementaion2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -26,7 +26,7 @@ exports.getCheckooutSession = catchAsync(async(req, res, next) => {
             {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
-                images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                 amount: tour.price * 100,
                 currency: 'usd',
                 quantity: 1
@@ -51,7 +51,7 @@ exports.getCheckooutSession = catchAsync(async(req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({email: session.customer_email})).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.dsiplay_items[0].amount / 100;
     await Booking.create({tour, user, price});
 }
 
@@ -63,7 +63,7 @@ exports.webhookCheckout = (req, res, next) => {
     }catch (err) {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
-    if(event.type ==='checkout.session.complete'){
+    if(event.type ==='checkout.session.completed'){
         createBookingCheckout(event.data.object);
     }
     res.status(200).json({received: true});
